682056d9f6d69a5048134a1130fdcf7d86bf2aec
tharanamayu <tharana.maya@gmail.com>
2018-12-30 22:05:16 +0530

    updated github finder

--- app.js
+++ app.js
@@ -1,33 +1,31 @@
-//Init Github
-const github=new Github;
-//Init UI
-const ui =new UI;
+// Init Github
+const github = new Github;
+// Init UI
+const ui = new UI;
 
-//Search input
-const searchUser =document.getElementById('searchUser');
+// Search input
+const searchUser = document.getElementById('searchUser');
 
-//search input event listener
-searchUser.addEventListener('keyup',(e)=>{
-  //Get input text
-  const userText =e.target.value;
+// Search input event listener
+searchUser.addEventListener('keyup', (e) => {
+  // Get input text
+  const userText = e.target.value;
 
-  if (userText!=='') {
-    //Make http call
-    github.getUser(userText)
-    .then(data=>{
-      if (data.profile.message ==='Not Found') {
-        //Show alert
-        ui.showAlert('user Not found','alert alert-danger');
-        
+  if(userText !== ''){
+   // Make http call
+   github.getUser(userText)
+    .then(data => {
+      if(data.profile.message === 'Not Found') {
+        // Show alert
+        ui.showAlert('User not found', 'alert alert-danger');
       } else {
-        //Show profile
+        // Show profile
         ui.showProfile(data.profile);
         ui.showRepos(data.repos);
       }
     })
-  } else{
-    //Clear profile
+  } else {
+    // Clear profile
     ui.clearProfile();
   }
-}
-);
+}); 
